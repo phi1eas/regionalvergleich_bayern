39a2fe88e7983475de0d24245dc8956db782ccce
Maximilian Siemers <siemersm@gmail.com>
2019-01-10 19:21:24 +0100

    Plot loop, Mama Besprechung, relevante Daten sortiert. todo: keep fixing plotting errors

--- analysis.R
+++ analysis.R
@@ -9,6 +9,9 @@ data = raw_data[!is.na(raw_data$Gemeinde.Schluessel.Nr), ]
 
 rm(raw_data)
 
+# Convert non-alphanumeric values to NA
+data[data == '•'] = NA
+
 # Load column names and insert them into data -----------------------------
 headers = read.csv('spaltennr_labels.csv', sep=',', header = F, stringsAsFactors = F)
 COLLABELS = as.list(headers[2, ])
@@ -54,7 +57,8 @@ data$Landkreis <- data$Regional.Schluessel %>%
 # Development -------------------------------------------------------------
 
 highlight = "Engelsberg"
-v = COLLABELS[5]
+# v = COLLABELS[5]
+v = COLLABELS$bevoelkerung.insgesamt
 Landkreis = "Traunstein"
 # myplot <- function(v, highlight, Landkreis = NULL) {
 v = as.character(v)
@@ -78,12 +82,45 @@ data %>%
   select(v) %>%
   mutate(!!v := ifelse(.[[v]] > upper_break, upper_break, .[[v]])) %>%
   ggplot(aes_string(x = v)) +
-  geom_histogram()
+  geom_histogram() +
+  geom_vline(xintercept = data %>%
+    filter(Bezeichnung == highlight) %>%
+    select(v) %>%
+    as.numeric())
 
 
-# ggplot(data %>% filter(Landkreis == Landkreis), aes_string(x = v)) +
-#   geom_histogram(breaks = breaks) +
-#   geom_vline(xintercept = data %>%
-#              filter(Bezeichnung == highlight) %>%
-#              select(v) %>%
-#              as.numeric())
+
+# loop --------------------------------------------------------------------
+highlight = "Engelsberg"
+Landkreis = "Traunstein"
+
+for(v in COLLABELS[52]) {
+  v = as.character(v)
+  
+  data.vector = data %>%
+    filter(Landkreis == Landkreis) %>%
+    select(v)
+  
+  data.vector = data.vector[!is.na(data.vector)] %>%
+    as.numeric
+  
+  lower_break = data.vector %>%
+    min()
+  upper_break <- data.vector %>%
+    quantile(0.99)
+  # quantile(1)
+  breaks = seq(lower_break, upper_break, length.out = 30)
+  
+  image = data %>%
+    filter(Landkreis == Landkreis) %>%
+    select(v) %>%
+    mutate(!!v := ifelse(.[[v]] > upper_break, upper_break, .[[v]])) %>%
+    ggplot(aes_string(x = v)) +
+    geom_histogram() +
+    geom_vline(xintercept = data %>%
+                 filter(Bezeichnung == highlight) %>%
+                 select(v) %>%
+                 as.numeric())
+  
+  ggsave(filename = paste0('plots/plot_', v, '.png'), plot = image, width = 10, height = 8)
+}
